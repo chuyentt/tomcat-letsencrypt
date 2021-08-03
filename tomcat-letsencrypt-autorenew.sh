80434d22a9822eb885477065b495f5ffa73c3a16
Chuyen <chuyentt@gmail.com>
2021-08-03 15:24:27 +0700

    Update tomcat-letsencrypt-autorenew.sh

--- tomcat-letsencrypt-autorenew.sh
+++ tomcat-letsencrypt-autorenew.sh
@@ -3,7 +3,7 @@ set -ex
 DOMAIN=""
 TOMCAT_KEY_PASS=""
 CERTBOT_BIN="/usr/local/bin/certbot-auto"
-EMAIL_NOTIFICATION="email_address"
+EMAIL_NOTIFICATION=""
 
 # Install certbot
 
@@ -16,33 +16,25 @@ install_certbot () {
 
 # Attempt cert renewal:
 renew_ssl () {
-    ${CERTBOT_BIN} renew  > /tmp/crt.txt
-    cat /tmp/crt.txt | grep "No renewals were attempted"
+    ${CERTBOT_BIN} renew  > /etc/letsencrypt/crt.txt
+    cat /etc/letsencrypt/crt.txt | grep "No renewals were attempted"
     if [[ $? -eq "0" ]]; then
         echo "Cert not yet due for renewal"
         exit 0
     else
 
-    # Create Letsencypt ssl dir if doesn't exist
-    echo "Renewing ssl certificate..."
+        # Create Letsencypt ssl dir if doesn't exist
+        echo "Renewing ssl certificate..."
 
-    # create a PKCS12 that contains both your full chain and the private key
-     rm -f /tmp/${DOMAIN}_fullchain_and_key.p12 2>/dev/null
-     openssl pkcs12 -export -out /tmp/${DOMAIN}_fullchain_and_key.p12 \
-       -passin pass:$TOMCAT_KEY_PASS \
-       -passout pass:$TOMCAT_KEY_PASS \
-       -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem \
-       -inkey /etc/letsencrypt/live/$DOMAIN/privkey.pem \
-       -name tomcat
- fi
+        # create a PKCS12 that contains both your full chain and the private key
+        rm -f /etc/letsencrypt/fullchain_and_key.p12 2>/dev/null
+        openssl pkcs12 -export -out /etc/letsencrypt/fullchain_and_key.p12 -passin pass:$TOMCAT_KEY_PASS -passout pass:$TOMCAT_KEY_PASS -in /etc/letsencrypt/live/${DOMAIN}/fullchain.pem -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -name tomcat
+    fi
  }
 
-      # Convert that PKCS12 to a JKS
-    rm -f /etc/ssl/${DOMAIN}.jks 2>/dev/null
-    keytool -importkeystore -deststorepass $TOMCAT_KEY_PASS -destkeypass $TOMCAT_KEY_PASS \
-      -destkeystore /etc/ssl/${DOMAIN}.jks -srckeystore /tmp/${DOMAIN}_fullchain_and_key.p12  \
-      -srcstoretype PKCS12 -srcstorepass $TOMCAT_KEY_PASS \
-      -alias tomcat
+# Convert that PKCS12 to a JKS
+rm -f /etc/letsencrypt/${DOMAIN}.jks 2>/dev/null
+keytool -importkeystore -deststorepass $TOMCAT_KEY_PASS -destkeypass $TOMCAT_KEY_PASS -destkeystore /etc/letsencrypt/${DOMAIN}.jks -srckeystore /etc/letsencrypt/fullchain_and_key.p12 -srcstoretype PKCS12 -srcstorepass $TOMCAT_KEY_PASS -alias tomcat
 
 # Send email notification on completion
 send_email_notification () {
@@ -50,17 +42,17 @@ send_email_notification () {
         echo " Retarting tomcat server"
         systemctl restart tomcat
         if [[ $? -eq "0" ]]; then
-            echo "" > /tmp/success
-            echo "Letsencrypt ssl certificate for $DOMAIN successfully renewed by cron job." >> /tmp/success
-            echo "" >> /tmp/success
-            echo "Tomcat successfully restarted after renewal" >> /tmp/success
-            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /tmp/success
+            echo "" > /etc/letsencrypt/success
+            echo "Letsencrypt ssl certificate for $DOMAIN successfully renewed by cron job." >> /etc/letsencrypt/success
+            echo "" >> /etc/letsencrypt/success
+            echo "Tomcat successfully restarted after renewal" >> /success
+            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /success
         else
-            echo "" > /tmp/failure
-            echo "Letsencrypt ssl certificate for $DOMAIN renewal by cron job failed." >> /tmp/failure
-            echo "" >> /tmp/failure
-            echo "Try again manually.." >> /tmp/failure
-            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /tmp/failure
+            echo "" > /etc/letsencrypt/failure
+            echo "Letsencrypt ssl certificate for $DOMAIN renewal by cron job failed." >> /etc/letsencrypt/failure
+            echo "" >> /etc/letsencrypt/failure
+            echo "Try again manually.." >> /etc/letsencrypt/failure
+            mail -s "$DOMAIN Letsencrypt renewal" $EMAIL_NOTIFICATION < /etc/letsencrypt/failure
         fi
     fi
 }
